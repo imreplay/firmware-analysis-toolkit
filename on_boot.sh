4e9bf801fb29927fdadbbf099f18ce25b48afd2b
Imreplay <imreplay@imreplay.com>
2020-09-24 17:34:52 +0900

    Update on_boot.sh

--- on_boot.sh
+++ on_boot.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 python3 /fat/reset.py
-
+/usr/sbin/sshd -D
 iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 80 -j DNAT --to 192.168.0.1:80
 iptables -t nat -A PREROUTING -p tcp -i eth0 --dport 443 -j DNAT --to 192.168.0.1:443
