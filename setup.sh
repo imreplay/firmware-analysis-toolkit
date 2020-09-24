745c2934b6adb6467e23707b10809da604d6470f
Imreplay <imreplay@imreplay.com>
2020-09-24 11:39:57 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,15 +4,10 @@ set -e
 sudo apt update
 sudo apt install -y python-pip python3-pip python3-pexpect unzip busybox-static fakeroot kpartx snmp uml-utilities util-linux vlan qemu-system-arm qemu-system-mips qemu-system-x86 qemu-utils
 
-echo "Installing binwalk"
-export DEBIAN_FRONTEND=noninteractive
-git clone --depth=1 https://github.com/devttys0/binwalk.git
-cd binwalk
-sudo ./deps.sh --yes
-sudo python3 ./setup.py install
+
 sudo -H pip3 install git+https://github.com/ahupp/python-magic
 sudo -H pip install git+https://github.com/sviehb/jefferson
-cd ..
+
 
 echo "Installing firmadyne"
 git clone --recursive https://github.com/firmadyne/firmadyne.git
